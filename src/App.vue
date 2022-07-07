8dbe53320f333ab5b30ec68633d0f1c211bc1643
nandohidayat <mnandohidayat@gmail.com>
2022-07-07 16:04:48 +0700

    intro to workcation

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <h1 class="text-4xl font-bold text-purple-500 text-center">Hello world!</h1>
+  <div class="h-screen w-screen bg-gray-100">
+    <img src="./assets/logo.svg" alt="Workcation" />
+  </div>
 </template>
 
 <style></style>
